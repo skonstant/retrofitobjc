fc49358ea4e13c182637edeea64eabfd8fb56241
stephane <stephane@voyageonline.co.uk>
2016-09-16 09:59:14 +0200

    github repo

--- spottshared/src/main/java/vol/Repo.java
+++ spottshared/src/main/java/vol/Repo.java
@@ -1,7 +1,35 @@
 package vol;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by stephane on 15/09/16.
  */
 public class Repo {
+
+	public enum Visibility {
+		all,
+		@SerializedName("public")
+		public_,
+		@SerializedName("private")
+		private_
+	}
+
+	public enum Type {
+		all, owner,
+		@SerializedName("public")
+		public_,
+		@SerializedName("private")
+		private_,
+		member
+	}
+
+	public enum Sort {
+		created, updated, pushed, full_name;
+	}
+
+	public Visibility visibility;
+	public String affiliation;
+	public Type type;
+	public Sort sort;
 }
